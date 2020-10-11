015ace714af4e36d87249f72b4b4f2ae31bd427a
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-10-11 12:53:25 +0100

    added first pass at forms testing

---
+++ tests/TestForms.php
@@ -0,0 +1,60 @@
+<?php // phpcs:ignore WordPress.Files.FileName
+/**
+ * Backyard forms testing.
+ *
+ * @package   backyard-foundation
+ * @author    Sematico LTD <hello@sematico.com>
+ * @copyright 2020 Sematico LTD
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://sematico.com
+ */
+
+namespace Backyard\Tests;
+
+use Backyard\Forms\FormBuilder;
+use Backyard\Forms\FormsServiceProvider;
+use Backyard\Plugin;
+use Backyard\Twig\TwigServiceProvider;
+use Backyard\Utils\Str;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Form;
+
+class TestForms extends \WP_UnitTestCase {
+
+	protected $form;
+
+	protected $plugin;
+
+	public function setUp() {
+		$form = ( new FormBuilder() )->create()
+			->add( 'task', TextType::class );
+
+		$this->form = $form;
+
+		$path   = realpath( __DIR__ . '/test-plugin', );
+		$plugin = new Plugin( $path, realpath( __DIR__ . '/test-plugin/test-plugin.php' ), 'config' );
+
+		$plugin->addServiceProvider( TwigServiceProvider::class );
+		$plugin->addServiceProvider( FormsServiceProvider::class );
+		$plugin->bootPluginProviders();
+
+		$this->plugin = $plugin;
+	}
+
+	public function testCanCreateForm() {
+		$this->assertInstanceOf( Form::class, $this->form );
+		$this->assertEquals( 1, $this->form->count() );
+		$this->assertTrue( $this->form->has( 'task' ) );
+	}
+
+	public function testTwigHasExtension() {
+		$this->assertTrue( $this->plugin->twig()->hasExtension( FormExtension::class ) );
+	}
+
+	public function testPluginCanRenderForm() {
+		$output = $this->plugin->twig()->render( 'form_example.twig', [ 'form' => $this->form->createView() ] );
+		$this->assertTrue( Str::contains( $output, '<form name="form"' ) );
+	}
+
+}
--- tests/test-plugin/config/example.php
+++ tests/test-plugin/config/example.php
@@ -1,6 +1,9 @@
 <?php
 
 $config = [
-	'test'       => 'value',
-	'views_path' => 'views',
+	'test'                    => 'value',
+	'views_path'              => 'views',
+	'default_forms_templates' => [
+		'forms.twig',
+	],
 ];
